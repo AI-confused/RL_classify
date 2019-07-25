d3b0f0ff21cdb8da2d453336b8146534fa0d7ce7
AI-confused <51109482+AI-confused@users.noreply.github.com>
2019-07-25 16:02:55 +0800

    Update Env.py

--- RL/Env.py
+++ RL/Env.py
@@ -19,7 +19,7 @@ train_data_list = []
 for i, _ in enumerate(reader):
     if i != 0:
         train_data_list.append(_)
-max_lable_count = 3631
+max_lable_count = 4025
 with open('dataset/count_train_button_type.json', 'r') as f:
     type_dic = json.load(f)
 # with open('apptype_train_vec.json', 'r') as f:
@@ -34,30 +34,32 @@ class env_classfy(object):
     def __init__(self):
         self.action_space = buttom_action_space
         self.n_actions = len(self.action_space)
-        self.point = 0
-        self.maxpoint = len(train_data_list)
+        # self.point = 0
+        # self.maxpoint = len(train_data_list)
         # print('maxpoint ' + str(self.maxpoint))
         self.done = 0
         self.right_counter = 0
         # self.ob_sum = 0
 
     def reset(self):
-        observation = np.array(json.loads(train_data_list[0][1]))
+        # observation = np.array(json.loads(train_data_list[0][0]))
         self.done = 0
-        self.point = 0
-        # self.labels
-        self.label = train_data_list[0][4]
-        # print(self.labels)
-        # for _ in contents[self.point]['type_id']:
-        #     self.labels.append(''.join(list(_)[:4]))
-        # print(self.labels)
-        # self.point += 1
-        return observation
+        self.minority_miss_count = 0
+        self.half_miss_count = 0
+    #     # self.point = 0
+    #     # self.labels
+    #     # self.label = train_data_list[0][1]
+    #     # print(self.labels)
+    #     # for _ in contents[self.point]['type_id']:
+    #     #     self.labels.append(''.join(list(_)[:4]))
+    #     # print(self.labels)
+    #     # self.point += 1
+    #     return observation
 
-    def step(self, action):
-        self.point += 1
-        if self.point == self.maxpoint-1:
-            self.done = 1
+    def step(self, action, label):
+        # self.point += 1
+        # if self.point == self.maxpoint-1:
+        #     self.done = 1
             # print(self.point)
         # if not self.done:
         type_minority_flag = 0
@@ -65,31 +67,39 @@ class env_classfy(object):
         type_half_flag = 0
         # for _ in self.labels:
             # if _:
-        if type_dic[self.label]/max_lable_count <= 0.1:
+        if type_dic[label]/max_lable_count <= 0.1:
             type_minority_flag = 1
-        elif type_dic[self.label]/max_lable_count > 0.1 and type_dic[self.label]/max_lable_count <= 0.5:
+        elif type_dic[label]/max_lable_count > 0.1 and type_dic[label]/max_lable_count <= 0.5:
             type_half_flag = 1
         else:
             type_majority_flag = 1
 
-        if buttom_action_space[action] == self.label:
-            if type_minority_flag:reward = 1
-            elif type_half_flag:reward = 0.5
-            elif type_majority_flag:reward = 0.1
+        if buttom_action_space[action] == label:
+            if type_minority_flag:
+                reward = 1
+            elif type_half_flag:
+                reward = 0.5
+            elif type_majority_flag:
+                reward = 0.1
         else:
             if type_minority_flag:
                 reward = -1
-                self.done = 1
-            elif type_half_flag:reward = -0.5
-            elif type_majority_flag:reward = -0.1
+                self.minority_miss_count += 1
+                # self.done = 1
+            elif type_half_flag:
+                reward = -0.5
+                self.half_miss_count += 1
+            elif type_majority_flag:
+                reward = -0.1
            # self.right_counter += 1
-
-        s_ = np.array(json.loads(train_data_list[self.point][1]))
+        if self.minority_miss_count >= 1000 or self.half_miss_count >= 5000:
+            self.done = 1
+        # s_ = np.array(json.loads(train_data_list[self.point][0]))
         # self.labels = []
-        self.label = train_data_list[self.point][4]
+        # self.label = train_data_list[self.point][1]
         # print(self.labels)
         # for _ in contents[self.point]['type_id']:
         #     self.labels.append(''.join(list(_)[:4]))
         # print(self.labels)
-        return s_, reward, self.done
+        return reward, self.done
 
